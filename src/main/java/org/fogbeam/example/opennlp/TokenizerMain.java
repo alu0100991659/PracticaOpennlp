c58c3278e372d8545cdb1054db5e2253ff6e7e20
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-10 17:03:37 +0000

    Añadiendo documentación en formato Doxygen

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -16,7 +16,8 @@ import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 import java.nio.file.*;
 
-
+/// Clase Tokenizer
+/** Clase para tokenizar una serie de archivos de entrada y exportar el resultado en un fichero de salida. */
 public class TokenizerMain
 {
 	public static void main( String[] args ) throws Exception
@@ -26,40 +27,37 @@ public class TokenizerMain
 		// InputStream modelIn = new FileInputStream( "models/en-token.bin" );
  
 		// the model we trained
-		InputStream modelIn = new FileInputStream( "models/en-token.model" );
+		InputStream modelIn = new FileInputStream( "models/en-token.model" ); ///< Modelo entrenado
 		
 		try
 		{
-			TokenizerModel model = new TokenizerModel( modelIn );
+			TokenizerModel model = new TokenizerModel( modelIn ); ///< Modelo del tokenizador
 			
-			Tokenizer tokenizer = new TokenizerME(model);
+			Tokenizer tokenizer = new TokenizerME(model); ///< Tokenizador
 			
 			/* 
 			 * Creo una lista con todos los archivos del diretorio donde
 			 * se encuentran los ficheros de entrada
 			 */
-			Stream<Path> walk = Files.walk(Paths.get(args[0]));
+			Stream<Path> walk = Files.walk(Paths.get(args[0])); ///< Stream con las path del directorio de entrada
 			List<String> inputFiles = walk.filter(Files::isRegularFile)
-					.map(x -> x.toString()).collect(Collectors.toList());
+					.map(x -> x.toString()).collect(Collectors.toList()); ///< Lista con los ficheros de entrada
 			walk.close();
 			
 			/*
 			 * Abro el fichero en dónde voy a imprimir los resultados de 
 			 * la tokenización
 			 */
-			FileOutputStream outputStream = new FileOutputStream(args[1]);
+			FileOutputStream outputStream = new FileOutputStream(args[1]); ///< Fichero de salida
 			
 			/* Recorro cada archivo de entrada */
 			for (int i = 0; i < inputFiles.size(); i++) {
 				
-				/* Guardo en un string el contenido del archivo actual */
-				String currentString = new String(Files.readAllBytes(Paths.get(inputFiles.get(i))));
+				String currentString = new String(Files.readAllBytes(Paths.get(inputFiles.get(i)))); ///< Fichero actual a tokenizar
 				
-				/* Tokenizo el string con el contenido del archivo actual */
-				String[] tokens = tokenizer.tokenize(currentString);
+				String[] tokens = tokenizer.tokenize(currentString); ///< Array con los tokens del fichero de entrada
 				
-				/* Escribo una cabecera con el nombre de cada archivo */
-				byte [] strHeader = ("------------------------- Tokens of file: "+inputFiles.get(i)+"\n").getBytes();
+				byte [] strHeader = ("------------------------- Tokens of file: "+inputFiles.get(i)+"\n").getBytes(); ///< Cabecera de cada archivo
 				outputStream.write(strHeader);
 				
 				/* Imprimo en el archivo de salida todos los tokens de la tokenizacion */
