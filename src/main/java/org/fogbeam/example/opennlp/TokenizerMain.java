98c6678397e12189d1b96ef70ae67c57fbc22cd7
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-10 16:22:08 +0000

    Entrada/Salida mediante ficheros realizada

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -3,12 +3,18 @@ package org.fogbeam.example.opennlp;
 
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.*;
 
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
+import java.nio.file.*;
 
 
 public class TokenizerMain
@@ -18,28 +24,52 @@ public class TokenizerMain
 		
 		// the provided model
 		// InputStream modelIn = new FileInputStream( "models/en-token.bin" );
-
-		
+ 
 		// the model we trained
 		InputStream modelIn = new FileInputStream( "models/en-token.model" );
 		
 		try
 		{
 			TokenizerModel model = new TokenizerModel( modelIn );
-		
+			
 			Tokenizer tokenizer = new TokenizerME(model);
 			
-				/* note what happens with the "three depending on which model you use */
-			String[] tokens = tokenizer.tokenize
-					(  "A ranger journeying with Oglethorpe, founder of the Georgia Colony, " 
-							+ " mentions \"three Mounts raised by the Indians over three of their Great Kings" 
-							+ " who were killed in the Wars.\"" );
+			/* 
+			 * Creo una lista con todos los archivos del diretorio donde
+			 * se encuentran los ficheros de entrada
+			 */
+			Stream<Path> walk = Files.walk(Paths.get(args[0]));
+			List<String> inputFiles = walk.filter(Files::isRegularFile)
+					.map(x -> x.toString()).collect(Collectors.toList());
+			walk.close();
 			
-			for( String token : tokens )
-			{
-				System.out.println( token );
+			/*
+			 * Abro el fichero en dónde voy a imprimir los resultados de 
+			 * la tokenización
+			 */
+			FileOutputStream outputStream = new FileOutputStream(args[1]);
+			
+			/* Recorro cada archivo de entrada */
+			for (int i = 0; i < inputFiles.size(); i++) {
+				
+				/* Guardo en un string el contenido del archivo actual */
+				String currentString = new String(Files.readAllBytes(Paths.get(inputFiles.get(i))));
+				
+				/* Tokenizo el string con el contenido del archivo actual */
+				String[] tokens = tokenizer.tokenize(currentString);
+				
+				/* Escribo una cabecera con el nombre de cada archivo */
+				byte [] strHeader = ("------------------------- Tokens of file: "+inputFiles.get(i)+"\n").getBytes();
+				outputStream.write(strHeader);
+				
+				/* Imprimo en el archivo de salida todos los tokens de la tokenizacion */
+				for( String token : tokens )
+				{
+					byte[] strToBytes = (token+"\n").getBytes();
+					outputStream.write(strToBytes);
+				}
 			}
-			
+			outputStream.close();
 		}
 		catch( IOException e )
 		{
